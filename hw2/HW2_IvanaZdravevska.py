a1afd1d1c12bdace53cec53f5531b1679e42c25d
ivanazdr12 <36411923+ivanazdr12@users.noreply.github.com>
2020-10-06 22:27:20 -0400

    Add files via upload

---
+++ hw2/HW2_IvanaZdravevska.py
@@ -0,0 +1,13 @@
+grades= ['A','C','F','A','D','D','B','C','B','D']
+frequency= [grades.count('A'), grades.count('B'), grades.count('C'), grades.count('D'), grades.count('F')]
+print('frequency = ', frequency)
+
+dog_breeds=['collie','sheepdog','Chow','Chihuahua']
+herding_dogs= (dog_breeds[:2])
+print(herding_dogs)
+
+tiny_dogs= ([dog_breeds[-1]])
+print(tiny_dogs)
+
+isln='Persian' in dog_breeds
+print(isln)
